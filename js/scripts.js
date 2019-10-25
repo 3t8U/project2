fced4f5658f228fa882d1be676cdfe918e73a038
Nate Powers <skynet@nathans-MacBook-Pro.local>
2019-10-25 14:26:27 -0700

    add some java

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,23 @@
-$(document).ready(function(){
-  
+$(document).ready(function() {
+
+  $(".form-control").submit(function(event) {
+    event.preventDefault();
+      var age = $("input#born").val();
+
+if (age > 21){
+  $(".ruby").show();
+  $(".rust, .swift").hide();
+}else if (age < 23) {
+  $(".rust").show();
+  $(".ruby, .swift").hide();
+}else if (age === 22) {
+  $(".swift").show();
+  $(".ruby, .rust").hide();
+} else {
+  console.log("error");
+}
+
+
+
+  });
+});
