91b9815fc1ceeb90feaebf730cbb53283331998e
Nate Powers <skynet@nathans-MacBook-Pro.local>
2019-11-02 15:15:51 -0700

    rebuild whole site from top to bottom

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,35 @@
 $(document).ready(function() {
-
   $(".form-control").submit(function(event) {
     event.preventDefault();
-    var iceCream = $("input#ice option:selected").val();
-    var jewels = document.getElementById('rocks').val();
-    var basic = document.getElementById('plain').val();
-    var comp = $(".type option:selected").val();
+
+
+    var iceCream = $ parseInt("input#icecream option:selected").val();
+    var macOr = $ parseInt("input#or option:selected").val();
+    var mineralsSelect = $ parseInt("input#minerals option:selected").val();
+    var semiColon = $ parseInt("input#semi-colon option:selected").val();
+    var basicSelect = $ parseInt("input#basic option:selected").val();
+
+
+    var total = iceCream + macOr + mineralsSelect + semiColon + basicSelect;
+console.log(total);
 
 
 
 
-
-
-if (iceCream === 1){
+if (total <= 3 ){
   $(".ruby").show();
   $(".rust, .swift").hide();
-}else if (iceCream === 2) {
+}else if (total >= 4 ) {
   $(".rust").show();
   $(".ruby, .swift").hide();
+} else (total = 6 ){
+  $(".swift").show();
+  $(".ruby, .rust").hide();
 }
 
+ var result = total
+$('#result').text(result);
+$("#result").show();
 
 
   });
