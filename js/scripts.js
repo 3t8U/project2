c91d13dba54bc83bfe554cd5ecb3e848755891e5
Nate Powers <skynet@nathans-MacBook-Pro.local>
2019-11-02 19:05:11 -0700

    make app work

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,43 @@
 
 //Frontend Logic
-
 $(document).ready(function() {
   $("form#question").submit(function(event) {
     event.preventDefault();
 
 
-    var iceCream = parseInt($("input#icecream option:selected").val());
-    var macOr = parseInt($("input#or option:selected").val());
-    var mineralsSelect = parseInt($("input#minerals option:selected").val());
-    var semiColon = parseInt($("input#semi-colon option:selected").val());
-    var basicSelect = parseInt($("input#basic option:selected").val());
+    var iceCream = parseInt($("select#icecream").val());
+    var macOr = parseInt($("select#or").val());
+    var mineralsSelect = parseInt($("select#minerals").val());
+    var semiColon = parseInt($("select#semi-colon").val());
+    var basicSelect = parseInt($("select#basic").val());
+
+// console.log(iceCream);
+// console.log(macOr);
+// console.log(mineralsSelect);
+// console.log(semiColon);
+// console.log(basicSelect);
+    //Backend logic
 
 
 
-//Backend logic
+    var total = iceCream + macOr + mineralsSelect + semiColon + basicSelect;
+    if (total <= 4){
+      alert ("please answer ALL the questions!");
+    } else if(total === 5 ){
+      $(".ruby").show();
+      $(".rust, .swift").hide();
+    } else if (total >= 6 && total <= 8 ) {
+      $(".rust").show();
+      $(".ruby, .swift").hide();
+    } else {
+      console.log(total);
+      $(".swift").show();
+      $(".ruby, .rust").hide();
+    }
 
-console.log(total);
-
- var total = (iceCream + macOr + mineralsSelect + semiColon + basicSelect);
-if (total <= 4){
-  alert ("please answer ALL the questions!");
-} else if(total = 5 ){
-  $(".ruby").show();
-  $(".rust, .swift").hide();
-}else if (total >= 6 && total <= 8 ) {
-  $(".rust").show();
-  $(".ruby, .swift").hide();
-}else (total >= 9 ) {
-  $(".swift").show();
-  $(".ruby, .rust").hide();
-}
-
- var result = total
-$('#result').text(result);
-$("#result").show();
+// console.log(total);
+    $('#answer').show();
+    // $("#result").show();
 
 
   });
